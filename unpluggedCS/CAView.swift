479e268fa7937fce675da146435207e9a3d43c6f
Ahmed-Ibrahim-3 <ahmed291204@gmail.com>
2024-11-05 19:26:10 +0000

    initial CA

--- unpluggedCS/CAView.swift
+++ unpluggedCS/CAView.swift
@@ -9,6 +9,36 @@ import SwiftUI
 struct CAView : View {
     var body : some View{
         Text("Computer Architecture")
+            .font(.title)
+        
+        HStack{
+            Text("""
+                We can break a computer down into four main structural components
+                \u{2022} Central Processing Unit (CPU) (often referred to as the **processor**
+                \u{2022} Main Memory (RAM)
+                \u{2022} I/O - Short for Input-Ouput, refers to data moving to or
+                    from devices connected to the computer. 
+                \u{2022} Some mechanism for communicating among the other 3 parts, 
+                    for example the *system bus*, a number of wires connecting to 
+                    all the components
+                Lets zoom in on the cpu and its individual cores 
+                """)
+            Image("cpu")
+                .resizable()
+                .frame(width: 350,height: 300)
+        }
+        Text("""
+            \u{2022} **Instruction logic**: This includes the tasks involved in 
+                fetching instructions, and decoding each instruction to determine
+                the operation and the memory locations of its operands
+            \u{2022} **Arithmetic and Logic Unit**: Performs the operations 
+                specified by an instructions 
+            \u{2022} **Load/Store Logic**: Manages the transfer of data to and 
+                from main memory via the cache.
+            """)
     }
 }
 
+#Preview{
+    CAView()
+}
